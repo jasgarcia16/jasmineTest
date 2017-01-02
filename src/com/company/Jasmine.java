8d9e3028f98812b03ad3905a2fccf615de995264
Corey Garcia <cgarcia@beachbody.com>
2017-01-01 21:53:57 -0800

    coding assignment #1

---
+++ src/com/company/Jasmine.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Jasmine {
+
+    private String firstName = "Jasmine";
+    private String lastName = "Garcia";
+    private Integer age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Testing Corey");
+        /**
+         * CODING ASSIGNMENT
+         * Write code so that the console output displays the below:
+         * "My name is Jasmine Garcia and I am 27 years old"
+         * NOTE- there is a new Jasmine class, I added it just now, and it has the methods you need to generate that output
+         */
+
+        //TODO: write code below...
     }
 }
