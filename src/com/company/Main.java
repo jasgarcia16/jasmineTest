67b4709ee5a35a36f129c02a12216bd9d2d5b7cd
Corey Garcia <cgarcia@beachbody.com>
2017-01-27 13:44:11 -0800

    Coding Assignment #3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,24 +5,19 @@ public class Main {
     public static void main(String[] args) {
         /**
          * CODING ASSIGNMENT
-         * Initialize a new String array.
-         * How to initialize a string array:    http://stackoverflow.com/questions/2564298/java-how-to-initialize-string
-         * Give the string array values of "Corey", "Diana", "Nathan", "Rocky", and "Ellie"
-         * Then generate a for loop, loop through each name in the string array, and print the name.
-         * For loop example:    http://stackoverflow.com/questions/6707695/iterate-through-string-array-in-java
+         * Create a new class called "JasmineMath"
+         * In the JasmineMath class, create 2 methods
+         * One method should be called "add".  It should accept 2 arguments (2 Integer numbers) and return an Integer.  The method should add the 2 arguments together, and then return the Integer result
+         * The other method should be called "subtract".  It should accept 2 arguments (2 Integer numbers) and return an Integer.  The method should subtract the 2 arguments together, and then return the result
+         * Once you have this done, initialize a new "JasmineMath" class, and call those methods with any numbers you want to demonstrate it works.  You will need to print the result to console.
+         * Note- look at the original "Jasmine" class. Look at the method called "public void setAge(Integer age)".  In this example, this accepts 1 argument (1 Integer number called age)
+         * Note- look at the original "Jasmine" class. Look at the method called "public Integer getAge()".  In this example, it returns an Integer.
+         * Note- public Integer methodName(). in this example, the return type will be an Integer.  it will return an Integer value when you call "return X" at the end of the method
+         * Note- public String methodName().  in this example, the return type will be a String.  it will return a String value when you call "return X" at the end of the method
+         * Note- public void methodName().  in this example, there is no return type.  it does not return anything. You cannot call return at the end of the method when it's void.
          */
 
         //TODO: write code below...
-
-        //I don't understand if I am doing the commit changes correctly...
-
-
-        String [] familyNames= {"Corey", "Diana", "Nathan", "Rocky", "Ellie"};
-
-        for (int x=0; x<familyNames.length;x++) {
-            System.out.println(familyNames[x]);
-        }
-
 
     }
 
